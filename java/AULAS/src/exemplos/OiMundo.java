060245ca91d11b731f1b1f697812413690308b89
EDNILSON-NASCIMENTO <ednilson.nascimento@mail.com>
2021-05-04 09:27:16 -0300

    primeiros exemplos em java turma 21

---
+++ java/AULAS/src/exemplos/OiMundo.java
@@ -0,0 +1,46 @@
+package exemplos;
+
+import java.util.Scanner;
+
+public class OiMundo {
+
+	public static void main(String[] args) {
+		
+		//variaveis
+		//inteiro numero1
+		//cadeia nome
+		//escreva("Digite o nome: )
+		//leia(nome)
+		
+		//instaciar = criar um teclado
+		Scanner leia = new Scanner(System.in);
+		
+		String nome; //cadeia
+		int numero1 = 10;
+		int numero2 = 30;
+		double salario = 1999.25;
+		char op = 'S';
+		
+		
+		System.out.println("Digite o nome: ");
+		nome = leia.next();
+		//escreva("Digite um numero: ")
+		System.out.println("Digite um numero 1: ");
+		//leia(numero1)
+		numero1 = leia.nextInt();
+		System.out.println("Digite o numero 2 :");
+		numero2 = leia.nextInt();
+		System.out.println("Digite o salario :");
+		salario = leia.nextDouble();
+		
+		System.out.println("nome: "+nome);
+		System.out.println("Salario :"+salario);
+		System.out.println(numero1+numero2);
+		
+		
+		
+		
+		
+	}
+	
+}
